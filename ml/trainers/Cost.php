dc3e99500e1696178b13bfafabe2d9436741e931
apronprof <nikita.pavlov.prof@gmail.com>
2025-05-03 13:17:47 +0200

    0.9

---
+++ ml/trainers/Cost.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ML\Trainers;
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../../consts.php';
+
+use Core\Classes\QueryBuilder;
+use Core\Classes\DB;
+use App\Models\House;
+
+
